6b4344daa750f03e97a69a0e266529e7f8be18e6
MrLawry <rlawry@southlewis.org>
2025-09-17 19:39:22 -0400

    period to comma

--- tools.js
+++ tools.js
@@ -21,7 +21,7 @@ const links = [
     { title: "Planet Size Comparison", url: "https://rlawry.github.io/planet-compare" },
     { title: "Doppler Shift of Celestial Spectra", url: "https://rlawry.github.io/spectra-redshift" },
     { title: "Parallax", url: "https://rlawry.github.io/parallax"},
-    { title: "HR-Diagram Plot", url: "https://rlawry.github.io/hr-diagram"}.
+    { title: "HR-Diagram Plot", url: "https://rlawry.github.io/hr-diagram"},
     { title: "Star Spectra", url: "https://rlawry.github.io/star-spectra"}
     // Add more links as needed
   ];
